fd1f08b5a7d71bc85b5726add181194b133e542f
aali2537 <alexander_li1@student.uml.edu>
2015-11-20 13:23:05 -0500

    Update table.js

--- table.js
+++ table.js
@@ -68,6 +68,7 @@
         tabs.tabs( "refresh" );
         tabCounter++;
         createTable(id);
+        console.log(tabCounter);
          $("#tabs").tabs("option", "active", tabCounter);
     }
       tabs.delegate( "span.ui-icon-close", "click", function() {
