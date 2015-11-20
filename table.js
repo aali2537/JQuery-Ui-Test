0a6c39263af3b6a6d6611f484b167f7898d1f058
aali2537 <alexander_li1@student.uml.edu>
2015-11-20 13:16:14 -0500

    Update table.js

--- table.js
+++ table.js
@@ -71,7 +71,7 @@
          $( "#tabs" ).tabs({ active: tabCounter });
     }
       tabs.delegate( "span.ui-icon-close", "click", function() {
-            console.log("#" + panelId);
+            console.log( $( "#" + panelId ));
             var panelId = $( this ).closest( "li" ).remove().attr( "aria-controls" );
             $( "#" + panelId ).remove();
             tabs.tabs( "refresh" );
